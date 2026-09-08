86f78903cdecc2d0d09a884135aa9293eda5d853
Siddhartha Maity <siddharthamaity1993@gmail.com>
2026-09-08 15:59:11 +0530

    feat: add GTM consent tracking

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const EXAMPLE_ORG_ID =
-  import.meta.env.VITE_ORGANIZATION_ID || '69ad9c7876d8bf6f864b3a65';
+  import.meta.env.VITE_ORGANIZATION_ID || '';
 
 export const GITHUB_ORG = 'qrati-labs';
 export const REPO = 'qrati-connect-react-example';
